94975f702ebb7cc3d9a172c7178eb81fd21fa2a0
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-13 21:16:12 -0600

    prompt, confirm, alert

--- 01-FUNDAMENTOS/assets/js/alerts.js
+++ 01-FUNDAMENTOS/assets/js/alerts.js
@@ -1,1 +1,8 @@
-console.log(miNombre);
+// alert('hola mundo');
+
+// let nombre = prompt('Cual es tu nombre?', 'Sin nombre');
+
+
+const seleccion = confirm('Estas seguro de borrar esto?');
+
+console.log(seleccion);
