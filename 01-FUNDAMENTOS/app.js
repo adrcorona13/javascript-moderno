49e221967d92f0ac5de11b467801168ab0f12618
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-13 19:29:32 -0600

    tipos de mensajes en la consola

--- 01-FUNDAMENTOS/app.js
+++ 01-FUNDAMENTOS/app.js
@@ -1,8 +1,23 @@
 let a = 10,
     b = 20,
-    c = 10,
-    d = 10,
+    c = 'hola ',
+    d = 'spiderman',
     x = a + b;
 
-    console.log(x);
     
+    console.log(x);
+    console.info(x);
+    console.warn(x);
+    console.error(x);
+
+    console.log('a', a);
+
+    console.log({a});
+
+    console.log('%c mis variables', 'color:blue; font-weight: bold');
+
+    console.table({a,b,c,d});
+
+const saludo = c + d;
+
+console.log(saludo);
