d93f0dd20961bec8ab941cdc44a17342b75f4ea0
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-14 22:30:09 -0600

    primitivos

---
+++ 01-FUNDAMENTOS/assets/js/primitivos.js
@@ -0,0 +1,31 @@
+let nombre = 'Cadena 1';
+console.log(nombre);
+nombre = 'Cadena 2';
+console.log(nombre);
+nombre = `Cadena 3`
+console.log(typeof nombre);
+nombre = 123; 
+console.log(nombre);
+
+let esMarvel = true;
+console.log(typeof esMarvel);
+
+let edad = 33;
+console.log(edad);
+edad = 33.45;
+console.log(edad);
+
+// camelCase
+let superPoderDeSpiderman;
+
+// snake_case
+let super_poder;
+console.log(super_poder);
+
+let soyNull = null;
+console.log(typeof soyNull);
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+console.log(typeof symbol1);
+console.log(symbol1 === symbol2);
