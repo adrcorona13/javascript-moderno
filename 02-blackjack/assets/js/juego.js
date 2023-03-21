1cef76a730687ce2528510c317bf2b2fcd532232
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-21 17:30:29 -0600

    obtener el valor de cada carta

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -30,7 +30,14 @@ const pedirCarta = () => {
     return deck.pop();
 };
 
+const valorCarta = (carta = '') => {
+    const valor = carta.substring(0, carta.length - 1);   
+    return isNaN(valor) ? 
+        valor === "A" ? 11 : 10 
+        : valor * 1 ;
+}
+
 crearDeck();
-console.log(deck.length);
-console.log(pedirCarta());
-console.log(deck.length);
+
+console.log(valorCarta(pedirCarta()));
+
