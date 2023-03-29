f15c6c3d2b82f528f2658c96f439b88bf1da7cd1
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-29 00:27:12 -0600

    get y set

--- 04-CLASES/assets/js/classes/class.js
+++ 04-CLASES/assets/js/classes/class.js
@@ -1,16 +1,25 @@
 
 class Persona {
 
-    nombre = '';
-    codigo = '';
-    frase = '';
-
+    nombre  = '';
+    codigo  = '';
+    frase   = '';
+    comida  = '';
+    
     constructor(nombre = 'sin nombre', codigo = 'sin codigo', frase = 'sin frase') {
         console.log('constructor');
 
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
+    }
+
+    set setComidaFavorita(comida){
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita(){
+        return `La comida favorita de ${this.codigo} es ${this.comida}`;
     }
 
     metodo1(){
@@ -24,8 +33,10 @@ class Persona {
 }
 
 const spiderman = new Persona();
-console.log(spiderman);
 const ironman = new Persona('a','b','c');
 console.log(ironman);
 spiderman.metodo2();
 
+spiderman.setComidaFavorita = 'pastel';
+spiderman.getComidaFavorita;
+console.log(spiderman.getComidaFavorita);
